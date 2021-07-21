21b3d6ae229202f89e923c490bfbe773eb8b749a
Christian <crica10andravilla@hotmail.com>
2021-07-21 14:36:16 -0500

    Base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB6Dl-3JHq5i-WLYtWXf6BOPJRkta8-RP8",
+    authDomain: "pedido-73289.firebaseapp.com",
+    projectId: "pedido-73289",
+    storageBucket: "pedido-73289.appspot.com",
+    messagingSenderId: "111391178417",
+    appId: "1:111391178417:web:b581add5aee7c9ef8c2c90",
+    measurementId: "G-BW02ZHWDZJ"
+  }
 };
 
 /*
